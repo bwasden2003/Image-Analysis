7c04835a67fb2ef257cc0043380e074be1535389
bwasden2003 <bwasden2003@gmail.com>
2023-05-17 15:55:16 -0500

    started analyzing the detected LFAs

--- LFA-Image_Analysis/detection.py
+++ LFA-Image_Analysis/detection.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import skimage as sk
 from skimage import io, color, filters
@@ -6,14 +7,12 @@ from skimage import morphology
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-global image
-global test_strips
+global image_obj
 global min_maxr, min_maxc
 
-def detect_lateral_flow_tests(image, line_length = 10):
-    
+def detect_lateral_flow_tests(image, lst, line_length = 10):
     # Increase gammma of the image
-    image = sk.exposure.adjust_gamma(image, 0.5)
+    image = sk.exposure.adjust_gamma(image, 0.4)
 
     # Create a binary mask based on color thresholding
     threshold = filters.threshold_otsu(image)
@@ -31,7 +30,9 @@ def detect_lateral_flow_tests(image, line_length = 10):
     eroded = morphology.binary_erosion(threshold_image, morphology.rectangle(line_length, 1))
     dilated = morphology.binary_dilation(eroded, morphology.rectangle(line_length, 1))
 
-    plt.imshow(dilated)
+    # io.imshow(dilated)
+    # plt.show()
+	
     # Label connected components in the binary mask
     labeled = label(dilated)
     background_color = np.mean(labeled[0:1, 0:1], axis=(0, 1))
@@ -47,9 +48,9 @@ def detect_lateral_flow_tests(image, line_length = 10):
         # Check if the mean color is lighter than the background color and the region is big enough to qualify as a test strip
         if mean_color > background_color and region.area >= 100:
             # Draw a rectangle around the detected test
-            if check_duplicate(minr, minc, maxr, maxc):
+            if check_duplicate(minr, minc, maxr, maxc, lst):
                 straighten_region(copy_image, region, angles)
-                test_strips.append(region)    
+                lst.append(region)    
     image = copy_image
 
 def straighten_region(copy_image, region, angles):
@@ -65,15 +66,15 @@ def straighten_region(copy_image, region, angles):
     region_angle = np.mean(region_angles)
 
     # Rotate the region to straighten it
-    rotated_region =sk.transform.rotate(image[minr:maxr, minc:maxc], region_angle, center=(center_row - minr, center_col - minc))
+    rotated_region = sk.transform.rotate(copy_image[minr:maxr, minc:maxc], region_angle, center=(center_row - minr, center_col - minc))
 
     # Replace the rotated region in the copy of the original image
     copy_image[minr:maxr, minc:maxc] = rotated_region
 
-def check_duplicate(minr, minc, maxr, maxc) -> bool:
+def check_duplicate(minr, minc, maxr, maxc, lst) -> bool:
     # Iterate through tests that have already been identified
-    for test in test_strips:
-        cur_minr, cur_minc, cur_maxr, cur_maxc = test.bbox
+    for region in lst:
+        cur_minr, cur_minc, cur_maxr, cur_maxc = region.bbox
         cur_maxr, cur_maxc = max(cur_maxr, cur_minr + min_maxr), max(cur_maxc, cur_minc + min_maxc)
         # Check whether the current region's dimensions overlap
         if (minr <= cur_maxr and maxr >= cur_minr and minc <= cur_maxc and maxc >= cur_minc):
@@ -82,25 +83,52 @@ def check_duplicate(minr, minc, maxr, maxc) -> bool:
 
 def draw_rectangle(minr, minc, maxr, maxc):
     # Create a rectangle patch
-    maxr, maxc = min(max(maxr, minr + min_maxr), len(image)), min(max(maxc, minc + min_maxc), len(image[0]))
     rect = patches.Rectangle((minc, minr), maxc - minc, maxr - minr, linewidth=2, edgecolor='r', facecolor='none')
     # Add the rectangle to the current plot
     plt.gca().add_patch(rect)
 
 
-image_path = 'images/image3.jpg'
-image = io.imread(image_path, as_gray=True)
-width = len(image[0])
-min_maxc = (int)(width / 40)
-min_maxr = (int)(min_maxc * 20)
-test_strips = []
-detect_lateral_flow_tests(image)
-for test in test_strips:
-    minr, minc, maxr, maxc = test.bbox
-    draw_rectangle(minr, minc, maxr, maxc)
+folder_path = 'images'
+# Iterate through images in folder
+for image in os.listdir(folder_path):
+    # Only consider jpg and jpeg for now
+    if image.endswith(('.jpg', '.jpeg')):
+        image_path = os.path.join(folder_path, image)
+        image_obj = io.imread(image_path, as_gray=True)
 
+        # Find width (in pixels) of the current image
+        width = len(image_obj[0])
 
-# Display the image with detected tests
-plt.imshow(image)
-plt.axis('off')
-plt.show()
+        # Use width to calculate the min size (in pixels) a LFA could be (this heavily depends on how the images are taken and will probably have to be reworked)
+        min_maxc = (int)(width / 40)
+        min_maxr = (int)(min_maxc * 20)
+
+        # Make a list for the test strip regions within the image
+        test_strips = []
+        detect_lateral_flow_tests(image_obj, test_strips)
+
+        # Go though test strips and make a new list of images of just the test strips
+        test_strip_images = []
+        for test in test_strips:
+            minr, minc, maxr, maxc = test.bbox
+            # Adjust indecies so that they are always in the bounds of the image
+            maxr, maxc = min(max(maxr, minr + min_maxr), len(image_obj)), min(max(maxc, minc + min_maxc), len(image_obj[0]))
+            test_strip_images.append(image_obj[minr:maxr, minc:maxc])
+        # for test in test_strips:
+        #     minr, minc, maxr, maxc = test.bbox
+        #     draw_rectangle(minr, minc, maxr, maxc)
+        for test in test_strip_images:
+            lines = []
+            detect_lateral_flow_tests(test, lines)
+            for line in lines:
+                minr, minc, maxr, maxc = line.bbox
+                draw_rectangle(minr, minc, maxr, maxc)
+            plt.imshow(test)
+            plt.show()
+
+		# Display the image with detected tests
+        # plt.imshow(image_obj)
+        # plt.axis('off')
+        # plt.show()
+    else:
+        continue
