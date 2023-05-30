123c1c9eef1f558d1ebcb9dafb288a46d1b67363
bwasden2003 <bwasden2003@gmail.com>
2023-05-30 14:55:51 -0500

    still trying to get control line detection consistent

--- LFA-Image_Analysis/detection.py
+++ LFA-Image_Analysis/detection.py
@@ -34,7 +34,7 @@ def detect_lateral_flow_tests(image, lst, line_length = 10):
 
     # io.imshow(dilated)
     # plt.show()
-	
+    
     # Label connected components in the binary mask
     labeled = label(dilated)
     background_color = np.mean(labeled[0:1, 0:1], axis=(0, 1))
@@ -109,39 +109,37 @@ def remove_outliers(lst):
     return new_lst
 
 def detect_lines(image):
-    
+
+    min_area = len(image[0])
+    max_area = len(image[0]) * 8
+
     fig = plt.figure(figsize=(10,7))
     fig.add_subplot(1, 2, 1)
     plt.imshow(image)
     image_copy = np.copy(image)
-    image_copy = sk.exposure.adjust_gamma(image_copy, 0.4)
-    
-    line_length = 2
-    # Rescale the intensity within the desired range
+    image_copy = sk.exposure.adjust_gamma(image_copy, 0.35)
     enhanced_image = sk.exposure.equalize_adapthist(image_copy)
-    p80, p99 = np.percentile(enhanced_image, (80, 92))
-    enhanced_image = sk.exposure.rescale_intensity(enhanced_image, in_range=(p80, p99))
-
+    p80, p99 = np.percentile(enhanced_image, (85, 98))
+    enhanced_image = sk.exposure.rescale_intensity(enhanced_image, in_range=(p80, p99), out_range=(0, 1))
+    
     thresholded_image = sk.filters.threshold_local(enhanced_image, block_size=3, method='gaussian', mode='reflect')
 
-    elevation_map = sk.filters.sobel(thresholded_image)
-
-    background_color = np.mean(image[len(image[0]) // 2: (len(image[0]) // 2) + 1, len(image) // 2: (len(image) // 2) + 1], axis=(0, 1))
+    # canny = sk.feature.canny(thresholded_image)
     # Label connected regions in the binary image
     labeled_image = sk.measure.label(thresholded_image)
     for region in regionprops(labeled_image):
         # Get the bounding box coordinates of the region
         minr, minc, maxr, maxc = region.bbox
-        # Calculate the mean color within the bounding box
-        mean_color = np.mean(image[minr:maxr, minc:maxc], axis=(0, 1))
-
-        # Check if the mean color is lighter than the background color and the region is big enough to qualify as a test strip
-        # if mean_color > background_color and (region.area > 20 and region.area < 70):
-            # Draw a rectangle around the detected test
+        if region.area > min_area:
+            minc = 0
+            maxc = len(image[0])
+        # Check if the region is big enough to qualify as a test strip
+        # Draw a rectangle around the detected test
+        area = (maxc - minc) * (maxr - minr)
+        # if ((maxc - minc) > (maxr - minr)) and area < max_area:
         draw_rectangle(minr, minc, maxr, maxc)
-
     fig.add_subplot(1, 2, 2)
-    plt.imshow(enhanced_image)
+    plt.imshow(thresholded_image)
     plt.show()
 
 angle_threshold = 10
@@ -150,8 +148,7 @@ region_num = 0
 # Iterate through images in folder
 for image in os.listdir(folder_path):
     # Only consider jpg and jpeg for now
-    if image.endswith(('.jpg', '.jpeg')):
-        region_num += 1
+    if image.endswith(('.jpg', '.jpeg')) and region_num == 2:
         image_path = os.path.join(folder_path, image)
         image_obj = io.imread(image_path, as_gray=True)
 
@@ -178,9 +175,8 @@ for image in os.listdir(folder_path):
         
         for test_image in test_strip_images:
             detect_lines(test_image)
-		# Display the image with detected tests
+        # Display the image with detected tests
         # plt.imshow(image_obj)
         # plt.axis('off')
         # plt.show()
-    else:
-        continue
+    region_num += 1
